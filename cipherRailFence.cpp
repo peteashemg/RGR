cfbe3628e7807d37f43d577d318ac2ffbc6a2013
1Navy4 <mihailromanenko24@gmail.com>
2026-06-12 19:30:41 +0900

    исправил обработку кодировки UTF 8 в шифре зигзаг с помощью широких символов

--- cipherRailFence.cpp
+++ cipherRailFence.cpp
@@ -1,48 +1,65 @@
 #include "cipherRailFence.h"
 #include <stdexcept>
+#include <windows.h> //добавил для функций конвертации кодировок
 
 using namespace std;
 
-//шифрование зигзагом (Rail fence)
+//функия конвертирует  UTF-8 байты в широкие символы
+wstring bytesToWstring(const vector<unsigned char>& data) {
+    if (data.empty()) return L"";
+    int size = MultiByteToWideChar(CP_UTF8, 0, (const char*)data.data(), data.size(), NULL, 0);
+    wstring result(size, 0);
+    MultiByteToWideChar(CP_UTF8, 0, (const char*)data.data(), data.size(), &result[0], size);
+    return result;
+}
+
+//функция возвращает широкие символы обратно в UTF-8 байты
+vector<unsigned char> wstringToBytes(const wstring& wstr) {
+    if (wstr.empty()) return {};
+    int size = WideCharToMultiByte(CP_UTF8, 0, wstr.data(), wstr.size(), NULL, 0, NULL, NULL);
+    vector<unsigned char> result(size);
+    WideCharToMultiByte(CP_UTF8, 0, wstr.data(), wstr.size(), (char*)result.data(), size, NULL, NULL);
+    return result;
+}
+
 vector<unsigned char> CipherRailFence::encrypt(const vector<unsigned char>& data, const string& key) {
     if (data.empty()) return data;
     
     int numRails = 0;
     try {
-        numRails = stoi(key); //переводим строку в число
+        numRails = stoi(key);
     } catch (...) {
-        throw invalid_argument("Ключ для шифра Зигзаг должен быть целым числом");
+        throw invalid_argument("Ключ для шифра Зигзаг должен быть целым числом!");
     }
-    
-    if (numRails <= 1) throw invalid_argument("Количество строк должно быть больше 1");
+    if (numRails <= 1) throw invalid_argument("Количество строк должно быть больше 1!");
 
-//массив векторов для построения зигзага
-    vector<vector<unsigned char>> fence(numRails);
+    //переводим байты в неделимые широкие символы
+    wstring wdata = bytesToWstring(data);
+    if (wdata.empty()) return data;
+
+    vector<vector<wchar_t>> fence(numRails);
     int rail = 0;
     bool directionDown = false;
 
-// распределяем байты по волне зигзага
-    for (size_t i = 0; i < data.size(); ++i) {
-        fence[rail].push_back(data[i]);
-        
+    //переставляем целые символы, а не отдельные байты
+    for (size_t i = 0; i < wdata.size(); ++i) {
+        fence[rail].push_back(wdata[i]);
         if (rail == 0 || rail == numRails - 1) {
-            directionDown = !directionDown; // Меняем направление движения
+            directionDown = !directionDown;
         }
         rail += directionDown ? 1 : -1;
     }
 
-//собираем строки в один итоговый вектор
-    vector<unsigned char> result;
-    result.reserve(data.size());
+    wstring wresult;
+    wresult.reserve(wdata.size());
     for (int i = 0; i < numRails; ++i) {
         for (size_t j = 0; j < fence[i].size(); ++j) {
-            result.push_back(fence[i][j]);
+            wresult.push_back(fence[i][j]);
         }
     }
-    return result;
+    return wstringToBytes(wresult);
 }
 
-//дешифрование методом зигзаг 
 vector<unsigned char> CipherRailFence::decrypt(const vector<unsigned char>& data, const string& key) {
     if (data.empty()) return data;
 
@@ -50,17 +67,18 @@ vector<unsigned char> CipherRailFence::decrypt(const vector<unsigned char>& data
     try {
         numRails = stoi(key);
     } catch (...) {
-        throw invalid_argument("Ключ для шифра Зигзаг должен быть целым числом!");
+        throw invalid_argument("Ключ для шифра Зигзаг должен быть целым числом");
     }
+    if (numRails <= 1) throw invalid_argument("Количество строк должно быть больше 1");
 
-    if (numRails <= 1) throw invalid_argument("Количество строк должно быть больше 1!");
+    wstring wdata = bytesToWstring(data);
+    if (wdata.empty()) return data;
 
-//матрица для разметки позиций символов
-    vector<vector<bool>> marker(numRails, vector<bool>(data.size(), false));
+    vector<vector<bool>> marker(numRails, vector<bool>(wdata.size(), false));
     int rail = 0;
     bool directionDown = false;
 
-    for (size_t i = 0; i < data.size(); ++i) {
+    for (size_t i = 0; i < wdata.size(); ++i) {
         marker[rail][i] = true;
         if (rail == 0 || rail == numRails - 1) {
             directionDown = !directionDown;
@@ -68,39 +86,31 @@ vector<unsigned char> CipherRailFence::decrypt(const vector<unsigned char>& data
         rail += directionDown ? 1 : -1;
     }
 
-// заполняем размеченный зигзаг элементами из зашифрованного вектора
-    vector<vector<unsigned char>> fence(numRails);
+    vector<vector<wchar_t>> fence(numRails);
     size_t dataIndex = 0;
     for (int i = 0; i < numRails; ++i) {
-        for (size_t j = 0; j < data.size(); ++j) {
-            if (marker[i][j] && dataIndex < data.size()) {
-                fence[i].push_back(data[dataIndex++]);
+        for (size_t j = 0; j < wdata.size(); ++j) {
+            if (marker[i][j] && dataIndex < wdata.size()) {
+                fence[i].push_back(wdata[dataIndex++]);
             }
         }
     }
 
-//собираем текст обратно , двигаясь по зигзагу
-    vector<unsigned char> result;
-    result.reserve(data.size());
+    wstring wresult;
+    wresult.reserve(wdata.size());
     vector<size_t> rIndex(numRails, 0);
     rail = 0;
     directionDown = false;
 
-    for (size_t i = 0; i < data.size(); ++i) {
-        result.push_back(fence[rail][rIndex[rail]++]);
+    for (size_t i = 0; i < wdata.size(); ++i) {
+        wresult.push_back(fence[rail][rIndex[rail]++]);
         if (rail == 0 || rail == numRails - 1) {
             directionDown = !directionDown;
         }
         rail += directionDown ? 1 : -1;
     }
-    return result;
+    return wstringToBytes(wresult);
 }
 
-//функции создания и удаления объектов класса
-CipherAPI* createCipher() {
-    return new CipherRailFence();
-}
-
-void releaseCipher(CipherAPI* cipher) {
-    delete cipher;
-}
+CipherAPI* createCipher() { return new CipherRailFence(); }
+void releaseCipher(CipherAPI* cipher) { delete cipher; }
